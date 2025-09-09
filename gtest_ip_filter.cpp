5c45c078da4a1a14655ea90aa4ef54b70511a014
KLMN\andreev_yv <243RhV2m>
2025-09-09 15:42:02 +0300

    Добавлены тесты для проверки методов TestSplit и TestIpToNumeric.

--- gtest_ip_filter.cpp
+++ gtest_ip_filter.cpp
@@ -1,5 +1,39 @@
 #define GOOGLE_TEST_MODULE GTEST_IP_FILTER
 
+#include <gtest/gtest.h>
+
 #include "ip_filter.h"
 
-#include <gtest/gtest.h>
+TEST(TestIPFilter, TestSplit)
+{
+    const std::string inputIPStirng = "112.154.12.32";
+    const std::vector<std::string> expected =
+    {
+        "112",
+        "154",
+        "12",
+        "32"
+    };
+
+    auto actual = split(inputIPStirng, '.');
+
+    ASSERT_EQ(expected.size(), actual.size());
+    for (int item = 0; item < expected.size(); i++)
+        ASSERT_EQ(expected[i], actual[i])
+}
+
+
+TEST(TestIPFilter, TestIpToNumeric)
+{
+    const std::vector<std::string> inputIPVector =
+    {
+        "112",
+        "154",
+        "12",
+        "32"
+    };
+    const uint32_t expected = 1889143840;
+
+    uint32_t actual = ip_to_numeric(inputIPVector);
+    ASSERT_EQ(expected, actual);
+}
